9bc2fe56756ade993d035e257dc7edad14cfa1bd
Janicxz <janizu99@gmail.com>
2025-02-10 02:04:47 +0200

    Add account system, add login/register UI on clientside

--- server.py
+++ server.py
@@ -1,23 +1,20 @@
 from pymongo import MongoClient
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+
 from flask_socketio import SocketIO, send, emit
+
 from datetime import datetime
+import hashlib
 
 app = Flask(__name__)
 app.config['SECRET'] = "secret123!"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 mongoclient = MongoClient("mongodb://localhost:27017/")
-chatlog_collection = ""
 db = mongoclient["mongoChat"]
 chatlog_collection = db.chatlog
-
-def init_mongodb():
-    pass
-    #chatlog_collection.insert_one({"username": "Server", "message": "Welcome to the chatroom!", "time": datetime.now().strftime("%H:%M")})
-
-def send_chatlog():
-    pass
+users_collection = db.users
+message_of_the_day = "Welcome to the chatroom!"
 
 @socketio.on('connect')
 def handle_connect(connectMsg):
@@ -27,7 +24,9 @@ def handle_connect(connectMsg):
         print("MongoDB connection failed, could not load chatlog")
         return
 
-    messages = chatlog_collection.find(limit=20, sort=[('time')])
+    # Load last 20 messages from chatlog database
+    messages = chatlog_collection.find(limit=20, sort=[('$natural', -1)])
+    msgArray = []
     for msg in messages:
         #print(msg)
         msgDict = {
@@ -35,19 +34,69 @@ def handle_connect(connectMsg):
             'message': msg['message'],
             'time': msg['time']
         }
-        emit("message", msgDict, broadcast=False)
+        msgArray.append(msgDict)
+    msgArray.append({'username': 'Server', 'message': message_of_the_day, 'time': datetime.now().strftime("[%H:%M]")})
+    # Reverse the array to send the messages in the correct order
+    for msg in msgArray[::-1]:
+        emit("message", msg, broadcast=False)
+    # Send a message to the user to scroll down the chatlog
+    send("scroll down", broadcast=False)
+    session['logged_in'] = False
+
+@socketio.on('register')
+def user_register(user):
+    username = user['username']
+    password = user['password']
+    password = hashlib.sha256(password.encode()).hexdigest()
+
+    print("User register attempt: " + username)
+
+    if users_collection.find_one({"username": username}) != None:
+        print("User already exists: " + username)
+        emit("register", {'success' : False}, broadcast=False)
+        return
+
+    users_collection.insert_one({"username": username, "password": password})
+    emit("register", {'success' : True}, broadcast=False)
+    print("User registered: " + username)
+    session['logged_in'] = True
+    session['username'] = username
+    password = ""
+
+@socketio.on('login')
+def user_login(user):
+    username = user['username']
+    password = user['password']
+    password = hashlib.sha256(password.encode()).hexdigest()
+    print("User login attempt: " + username)
+
+    if users_collection.find_one({"username": username, "password": password}) != None:
+        emit("login", {'logged_in' : True}, broadcast=False)
+        print("User logged in: " + username)
+        session['logged_in'] = True
+        session['username'] = username
+    else:
+        emit("login", {'logged_in' : False}, broadcast=False)
+        print("User failed to login: " + username)
+        session['logged_in'] = False
+    password = ""
 
 @socketio.on('message')
 def handle_message(message):
     if message == "User connected!":
-        # Send user the recent chatlog on server
-        send_chatlog()
+        return
+    
+    if session['logged_in'] == False:
+        print("User not logged in, message not sent")
         return
 
     time = "[" + datetime.now().strftime("%H:%M") + "] "
-    username = message['username']
+    username = session['username']#message['username']
     message = message['message']
     print("Message received: " + time + " " + username + ": " + message)
+
+    if username == "" or message == "":
+        return
 
     # Save message to MongoDB
     try:
@@ -66,5 +115,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    init_mongodb()
     socketio.run(app, host="localhost")
